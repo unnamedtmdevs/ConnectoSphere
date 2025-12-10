51c116b0a1d8be813202cf5ebd34791a8ff9922e
ludik <bakhanetssimon@gmail.com>
2025-12-10 17:49:29 +0300

    Initial Commit

---
+++ ConnectoSphere/ConnectoSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectoSphereApp.swift
+//  ConnectoSphere
+//
+//  Created by Simon Bakhanets on 10.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ConnectoSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConnectoSphereTests/ConnectoSphereTests.swift
@@ -0,0 +1,16 @@
+//
+//  ConnectoSphereTests.swift
+//  ConnectoSphereTests
+//
+//  Created by Simon Bakhanets on 10.12.2025.
+//
+
+import Testing
+
+struct ConnectoSphereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
